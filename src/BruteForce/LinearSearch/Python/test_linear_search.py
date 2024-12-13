9a49d8db82015a25e01d5e19c1e9641e27c3e612
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-12-13 21:59:49 +0530

    1st algorithm

---
+++ src/BruteForce/LinearSearch/Python/test_linear_search.py
@@ -0,0 +1,24 @@
+import unittest
+from linear_search import linear_search
+
+
+class TestLinearSearch(unittest.TestCase):
+    def test_found(self):
+        self.assertEqual(linear_search([10, 23, 45, 70, 11, 15], 45), 2)
+        self.assertEqual(linear_search([1, 2, 3, 4, 5], 1), 0)
+        self.assertEqual(linear_search([1, 2, 3, 4, 5], 5), 4)
+
+    def test_not_found(self):
+        self.assertEqual(linear_search([10, 23, 45, 70, 11, 15], 99), -1)
+        self.assertEqual(linear_search([], 10), -1)
+
+    def test_duplicates(self):
+        self.assertEqual(linear_search([1, 2, 3, 3, 4], 3), 2)
+
+    def test_single_element(self):
+        self.assertEqual(linear_search([5], 5), 0)
+        self.assertEqual(linear_search([5], 1), -1)
+
+
+if __name__ == "__main__":
+    unittest.main()
